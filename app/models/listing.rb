584ea5503839de1cd706149b24d9c40409c355bd
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-24 15:47:22 +0800

    Fix : fixe the seeds, and photos

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,8 +3,10 @@ class Listing < ApplicationRecord
   has_many :rentals
   has_many :reviews, through: :rentals
 
-  validates :title, presence: true, length: { maximum: 15 }
-  validates :description, length: { minimum: 30, maximum: 500 }
+  has_one_attached :photo
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, length: { minimum: 10, maximum: 500 }
   validates :category, presence: true
   validates :size, presence: true
   validates :price, presence: true
