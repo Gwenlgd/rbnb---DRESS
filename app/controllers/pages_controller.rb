fed2a4ef0e8c8093721748f8a98f77c3d26ba706
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-26 16:08:22 +0800

    Done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def dashboard
+    @rentals = current_user.rentals
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :rentals, dependent: :destroy
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone_number, presence: true
-  validates :address, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :phone_number, presence: true
+  # validates :address, presence: true
 end
