22d16657707501c9d448a682330b88f1331889ec
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-09-26 15:43:07 +0200

    Fix: problem display & change the way it's display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/dashboard', to: 'pages#dashboard'
+  get '/tests', to: 'pages#testgwen'
+
+
   resources :listings do
     resources :rentals, except: [:edit, :update]
   end
